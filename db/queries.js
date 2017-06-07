cb6eb9b244a74c4a485caa23a4e280fd7c6a0ab3
Murph Grainger <murph.grainger@gmail.com>
2017-06-07 12:10:38 -0600

    linked user to post with dummy 1

--- db/queries.js
+++ db/queries.js
@@ -18,13 +18,33 @@ module.exports = {
       .query()
       .skipUndefined();
   },
-  
+
+  // addPost: function(post) {
+  //   console.log(post);
+  //   let str = post.date;
+  //   let date = moment(str);
+  //   let formattedDate = date.utc().format('ddd, MMM Do');
+  //   return knex('post')
+  //     .insert({
+  //       date: post.date,
+  //       handicap_min: post.handicapRange[0],
+  //       handicap_max: post.handicapRange[1],
+  //       age_min: post.ageRange[0],
+  //       age_max: post.ageRange[1],
+  //       gender: post.gender,
+  //       group_count: post.sizeGroup,
+  //       address: post.address
+  //     });
+  // },
+
   addPost: function(post) {
     console.log(post);
-    let str = post.date;
-    let date = moment(str);
-    let formattedDate = date.utc().format('ddd, MMM Do');
-    return knex('post')
+    return User
+    .query()
+    .findById(post.currentUserId)
+    .then(user => {
+      return user
+      .$relatedQuery('posts')
       .insert({
         date: post.date,
         handicap_min: post.handicapRange[0],
@@ -35,5 +55,6 @@ module.exports = {
         group_count: post.sizeGroup,
         address: post.address
       });
+    });
   }
 };
