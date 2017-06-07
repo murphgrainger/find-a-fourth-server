ef9f88b62d733d396dddc1dfc65b45c2672156dd
Murph Grainger <murph.grainger@gmail.com>
2017-06-07 12:45:53 -0600

    initial unauthorized middleware added

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var cors = require('cors')
-var index = require('./routes/index');
-var users = require('./routes/users');
-var app = express();
+
+const cors = require('cors')
+const index = require('./routes/index');
+const users = require('./routes/users');
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
+
+
 
 app.use('/', index);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ var fetch = require('node-fetch');
 var router = express.Router();
 var Q = require('../db/queries');
 
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
+
 
 router.get('/posts', function(req, res, next) {
   Q.getPosts()
@@ -15,7 +18,21 @@ router.get('/posts', function(req, res, next) {
   });
 });
 
-router.get('/users', function(req, res, next) {
+const authCheck = jwt({
+  secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        // YOUR-AUTH0-DOMAIN name e.g prosper.auth0.com
+        jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+    }),
+    // This is the identifier we set when we created the API
+    audience: `${process.env.AUTH0_API_IDENTIFIER}`,
+    issuer: process.env.AUTH0_DOMAIN,
+    algorithms: ['RS256']
+});
+
+router.get('/users', authCheck, function(req, res, next) {
   Q.getUsers()
   .then(response => {
     console.log(response);
